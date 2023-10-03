bba83c9f130dd42eee0694431678aa9ef1b018d6
Rinaxdz <91366054+Rinaxdz@users.noreply.github.com>
2023-10-03 15:07:38 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+let moves = [];
+let moveCount = 0;
+
+function solve() {
+    moves = [];
+    moveCount = 0;
+    const numDisks = parseInt(document.getElementById('numDisks').value);
+    towerOfHanoi(numDisks, 'A', 'C', 'B');
+    animateMoves();
+}
+
+function towerOfHanoi(n, source, target, auxiliary) {
+    if (n === 1) {
+        moves.push({ source, target });
+        return;
+    }
+
+    towerOfHanoi(n - 1, source, auxiliary, target);
+    moves.push({ source, target });
+    towerOfHanoi(n - 1, auxiliary, target, source);
+}
+
+function animateMoves() {
+    let delay = 0;
+    const moveList = document.getElementById('moveList');
+    
+    moves.forEach((move, index) => {
+        const disk = document.querySelector(`#${move.source} .disk`);
+        const targetTower = document.getElementById(move.target);
+
+        setTimeout(() => {
+            targetTower.appendChild(disk);
+            moveCount++;
+            updateMoveList(moveCount, move.source, move.target);
+        }, index * 1000); 
+    });
+}
+
+function updateMoveList(moveNumber, source, target) {
+    const moveList = document.getElementById('moveList');
+    const listItem = document.createElement('li');
+    listItem.textContent = `Move ${moveNumber}: Move a disk from ${source} to ${target}`;
+    moveList.appendChild(listItem);
+}
+
+document.getElementById('towerForm').addEventListener('submit', function (e) {
+    e.preventDefault(); 
+    solve();
+});
+
+
+solve();
